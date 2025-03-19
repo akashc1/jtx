59ef6f4a0c5d76c4ae381645201f99e5ed304927
Akash Chaurasia <akash@hippocraticai.com>
2025-03-19 20:30:09 +0000

    flax/optax deps

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+import jax
+from flax import nnx
+from typing import Optional
+class MLP(nnx.Module):
+    def __init__(self, dim: int, lora_rank: Optional[int] = None):
+        self.dim = dim
+        self.lora_rank = lora_rank
+
+    def __call__(self, x):
+        x = nnx.LoRALinear()
+        x = nnx.nn.relu(x)
+        x = nnx.nn.Dense(features=10)(x)
+        return x
+
 def main():
+    devices = jax.devices()
+    print(devices)
     print("Hello from jtx!")
 
 
